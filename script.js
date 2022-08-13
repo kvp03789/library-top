3f276b6038daab0c5df937ed09db5aca1037011e
clay <clay.nichols@gmail.com>
2022-08-13 02:16:08 -0500

    Create display for books that are created.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let popup = document.querySelector(".popup-container");
 let closeButton = document.querySelector("#close");
 let saveButton = document.querySelector("#save-book")
 
+
+
+
 //open and close popup
 addBookButton.addEventListener("click", () => {
     popup.classList.remove("hidden");
@@ -24,15 +27,16 @@ saveButton.addEventListener("click", (event) => {
     let readValue = document.querySelector('input[name="read"]:checked').value;
 
     var newBook = new Book(titleValue, authorValue, pagesValue, readValue);
-    console.log(newBook);
     library.push(newBook);
     popup.classList.remove("block");
     popup.classList.add("hidden");
     console.log(library);
+
+    addBookToLibrary();
 })
 
 
-
+//constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -40,8 +44,34 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
-// function addBookToLibrary(...arg) {
-//     let newBook = new Book(...arg)
-//     library.push(newBook);
-// }
+
+
+//create div, give class
+function addBookToLibrary() {
+    let bookShelf = document.querySelector(".book-shelf");
+    let bookDiv = document.createElement("div");
+    let bookTitle = document.createElement("h3");
+    let bookAuthor = document.createElement("p");
+    let bookPages = document.createElement("p");
+    let removeButton = document.createElement("button");
+
+    bookDiv.classList.add("book-div");
+    removeButton.classList.add("remove-button")
+    removeButton.innerHTML = "Remove";
+
+    removeButton.addEventListener("click", (e) => {
+        library.splice(library.indexOf(e), 1);
+    })
+
+    library.forEach(item => {
+        bookTitle.innerHTML = item.title;
+        bookAuthor.innerHTML = item.author;
+        bookPages.innerHTML = item.pages;
+    })
+
+
+    bookDiv.append(bookTitle, bookAuthor, bookPages, removeButton);
+    bookShelf.appendChild(bookDiv);
+
+}
 
